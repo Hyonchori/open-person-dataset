c5b46bad015f7fad83eb29bb5e745132299fb725
root <gus8cjf@naver.com>
2022-03-10 14:22:29 +0900

    resize 2 letterbox

--- PTAW/PTAW172Real/extract_frames_multithread.py
+++ PTAW/PTAW172Real/extract_frames_multithread.py
@@ -6,6 +6,7 @@ import multiprocessing as mp
 from pathlib import Path
 
 import cv2
+import numpy as np
 
 
 def extract_frames(vid_item, verbose=False):
@@ -15,7 +16,7 @@ def extract_frames(vid_item, verbose=False):
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    resize = [width, height] == target_size
+    resize = [width, height] != target_size
     if verbose:
         print(f"\n--- Processing {vid_name}")
         fps = cap.get(cv2.CAP_PROP_FPS)
@@ -29,10 +30,43 @@ def extract_frames(vid_item, verbose=False):
         if save and tmp_frame % interval == 0:
             save_path = os.path.join(save_dir, Path(vid_name).name.replace(".mp4", "") + f"_{tmp_frame}.png")
             if resize:
-                img = cv2.resize(img, dsize=target_size)
+                img = letterbox(img, target_size[::-1], auto=False)[0]
             cv2.imwrite(save_path, img)
             cnt += 1
     print(f"\ttotal {cnt} frames are saved from {Path(vid_name).name}!")
+
+
+def letterbox(im, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True, stride=32):
+    # Resize and pad image while meeting stride-multiple constraints
+    shape = im.shape[:2]  # current shape [height, width]
+    if isinstance(new_shape, int):
+        new_shape = (new_shape, new_shape)
+
+    # Scale ratio (new / old)
+    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+    if not scaleup:  # only scale down, do not scale up (for better val mAP)
+        r = min(r, 1.0)
+
+    # Compute padding
+    ratio = r, r  # width, height ratios
+    new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
+    if auto:  # minimum rectangle
+        dw, dh = np.mod(dw, stride), np.mod(dh, stride)  # wh padding
+    elif scaleFill:  # stretch
+        dw, dh = 0.0, 0.0
+        new_unpad = (new_shape[1], new_shape[0])
+        ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]  # width, height ratios
+
+    dw /= 2  # divide padding into 2 sides
+    dh /= 2
+
+    if shape[::-1] != new_unpad:  # resize
+        im = cv2.resize(im, new_unpad, interpolation=cv2.INTER_LINEAR)
+    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+    im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
+    return im, ratio, (dw, dh)
 
 
 def main(args):
@@ -78,7 +112,7 @@ def parse_args():
     target_vids = "/media/daton/Data/datasets/PTAW/PTAW_Datasets/PTAW172Real/extracted_videos"
     parser.add_argument("--target-videos", type=str, default=target_vids)
 
-    save_dir = "/media/daton/Data/datasets/PTAW/PTAW_Datasets/PTAW172Real/extracted_frames_by_interval"
+    save_dir = "/media/daton/Data/datasets/PTAW/PTAW_Datasets/PTAW172Real/extracted_frames_by_interval2"
     parser.add_argument("--save-dir", type=str, default=save_dir)
 
     target_size = [1280, 720]
